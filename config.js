9275c96bea80ca277f1d0aafa0f173725f847066
PC28K <sky162178@gmail.com>
2023-02-21 21:51:11 +0900

    indexで上に出す文字列を変更

--- config.js
+++ config.js
@@ -1,7 +1,11 @@
 //DB一覧(Array)
 const dbList = {
-    'サンプルデータ': './sample.json'
+    'サンプルデータ': './sample.json',
+	'新サンプル': './sample2.json'
 }
 
 //通知更新周期
 const alertRefreshDelay = 1000;
+
+//メインのタイトル
+const mainTitle = 'キミは素敵なハト';
